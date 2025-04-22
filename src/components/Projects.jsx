6093175b0a95abf9024c6d34f094a9eb9d7c799d
divyanshuaaliwal <divyanshuaaliwal@gmail.com>
2025-04-22 23:27:43 +0530

    Projects Done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
 import { PROJECTS } from "../constants";
+import { motion } from 'framer-motion';
 
 const Projects = () => {
     return (
         <div className="pb-4">
-            <h2 className="my-20 text-center text-4xl">Projects</h2>
+
+            <motion.h2 
+                whileInView={{opacity:1, y:0}}
+                initial={{opacity:0, y:-100}}
+                transition={{duration:0.5}}
+                className="my-20 text-center text-4xl"
+            >
+                Projects
+            </motion.h2>
+            
             <div>
                 {
                     PROJECTS.map((project, index) => (
@@ -13,7 +23,12 @@ const Projects = () => {
                         
                         <div key={index} className="mb-8 flex flex-wrap lg:gap-x-4 lg:justify-center">
 
-                            <div className="w-full lg:w-1/4">
+                            <motion.div 
+                                whileInView={{opacity:1, x:0}}
+                                initial={{opacity:0, x:-100}}
+                                transition={{duration:1}}
+                                className="w-full lg:w-1/4"
+                            >
                                 <img
                                     src={project.image}
                                     width={250}
@@ -21,9 +36,14 @@ const Projects = () => {
                                     alt={project.title}
                                     className="mb-6 rounded"
                                 />
-                            </div>
+                            </motion.div>
 
-                            <div className="w-full max-w-xl lg:w-3/4">
+                            <motion.div     
+                                whileInView={{opacity:1, x:0}}
+                                initial={{opacity:0, x  :100}}
+                                transition={{duration:1}}
+                                className="w-full max-w-xl lg:w-3/4"
+                            >
 
                                 <h3 className="mb-2 font-semibold text-2xl">
                                     {project.title}
@@ -43,7 +63,7 @@ const Projects = () => {
                                         ))
                                     }
                                 </div>
-                            </div>
+                            </motion.div>
                         </div>
                     ))
                 }
