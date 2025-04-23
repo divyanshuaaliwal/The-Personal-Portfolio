5157704e5d203429d4ccb1fb93b583104d6897ca
divyanshuaaliwal <divyanshuaaliwal@gmail.com>
2025-04-23 23:14:40 +0530

    Bugs Fixed

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -41,16 +41,19 @@ const Experience = () => {
                                     </span>
                                 </h3>
                                 <p className="mb-4 text-stone-400">{experience.description}</p>
+                                
+                                <div className="flex flex-wrap gap-2 mb-6">
                                 {
                                     experience.technologies.map((tech, index) => (
                                         <span
-                                            className="mr-2 mt-4 rounded bg-stone-900 px-2 py-1 text-sm font-medium text-stone-300"
+                                            className="rounded bg-stone-900 p-2 text-sm font-medium text-stone-300"
                                             key={index}
                                         >
                                             {tech}
                                         </span>
                                     ))
                                 }
+                                </div>
                             </motion.div>
                         </div>
                     ))
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import profilePic from "../assets/divyanshuaaliwal.png"
 import { HERO_CONTENT } from "../constants"
-
 import { motion } from 'framer-motion';
 
 const containerVariants = {
@@ -27,6 +26,25 @@ const childVariants = {
 
 
 const Hero = () => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const toggleReadMore = () => setIsExpanded(!isExpanded);
+
+    const getText = () => {
+        if (isExpanded || windowWidth >= 768) return HERO_CONTENT;
+        return HERO_CONTENT.slice(0, 250) + "...";
+    };
+
+    useEffect(() => {
+        const handleResize = () => setWindowWidth(window.innerWidth);
+
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup the event listener when the component unmounts
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <div className="pb-4 lg:mb-36">
             <div className="flex flex-wrap lg:flex-row-reverse">
@@ -38,41 +56,47 @@ const Hero = () => {
                             className="border border-stone-900 rounded-3xl"
                             width={650}
                             height={650}
-                            initial={{x:100, opacity:0}}
-                            animate={{x:0, opacity:1}}  
-                            transition={{duration:1, delay:1.5}}
+                            initial={{ x: 100, opacity: 0 }}
+                            animate={{ x: 0, opacity: 1 }}
+                            transition={{ duration: 1, delay: 1.5 }}
                         />
                     </div>
                 </div>
                 <div className="w-full lg:w-1/2">
-                    <motion.div 
+                    <motion.div
                         initial="hidden"
                         animate="visible"
                         variants={containerVariants}
                         className="flex flex-col items-center lg:items-start mt-10"
                     >
-                        <motion.h2 
+                        <motion.h2
                             className="pb-2 text-4xl tracking-tighter lg:text-8xl"
                             variants={childVariants}
                         >
                             Divyanshu Aaliwal
                         </motion.h2>
 
-                        <motion.span 
+                        <motion.span
                             variants={childVariants}
                             className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent"
                         >
                             A Frontend Developer
                         </motion.span>
-                        
-                        <motion.p 
-                            variants={childVariants}
-                            className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter"
-                        >
-                            {HERO_CONTENT}
-                        </motion.p>
-                        
-                        <motion.a href='/resume.pdf' target='_blank' rel='noopener-noreferrer' 
+
+                        <motion.div variants={childVariants}>
+                            <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
+                                {getText()}
+                                {isExpanded ? " " : ""}
+                                <button
+                                    onClick={toggleReadMore}
+                                    className="rounded bg-stone-900 p-1/2 text-sm font-medium text-stone-300 md:hidden"
+                                >
+                                    {isExpanded ? "Read less" : "Read more"}
+                                </button>
+                            </p>
+                        </motion.div>
+
+                        <motion.a href='/resume.pdf' target='_blank' rel='noopener-noreferrer'
                             variants={childVariants}
                             className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'
                             download
@@ -83,7 +107,7 @@ const Hero = () => {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Hero;
